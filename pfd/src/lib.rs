225c96b3d1484a4a0ae2a14dbf20df500e85d7cc
rektide <rektide+scm@voodoowarez.com>
2026-01-16 07:07:10 -0500

    Switch from UnixStream to UnixDatagram (DGRAM) for simpler fire-and-forget client model

--- pfd/src/lib.rs
+++ pfd/src/lib.rs
@@ -1,7 +1,9 @@
 use anyhow::Result;
 use discovery::{CreateStrategy, LocalFileStrategy};
 use sendfd::RecvWithFd;
-use tokio::net::UnixListener;
+use std::os::fd::{FromRawFd, OwnedFd};
+use std::os::unix::io::AsRawFd;
+use tokio::net::UnixDatagram;
 use tokio::signal;
 
 pub async fn run_daemon() -> Result<()> {
@@ -12,26 +14,36 @@ pub async fn run_daemon() -> Result<()> {
 
     std::fs::remove_file(&socket_path).ok();
 
-    let listener = UnixListener::bind(&socket_path)?;
+    let socket = UnixDatagram::bind(&socket_path)?;
     tracing::info!("Listening on {}", socket_path);
 
     let ctrl_c = signal::ctrl_c();
     tokio::pin!(ctrl_c);
 
+    let raw_fd = socket.as_raw_fd();
+
     loop {
         tokio::select! {
-            result = listener.accept() => {
+            result = tokio::task::spawn_blocking({
+                let mut buf = [0u8; 1024];
+                let mut fd_storage = [0; 8];
+                move || {
+                    use std::os::unix::net::UnixDatagram as StdUnixDatagram;
+                    let std_socket = unsafe {
+                        StdUnixDatagram::from(OwnedFd::from_raw_fd(raw_fd))
+                    };
+                    std_socket.recv_with_fd(&mut buf, &mut fd_storage)
+                }
+            }) => {
                 match result {
-                    Ok((stream, _addr)) => {
-                        tracing::debug!("Accepted connection");
-                        tokio::spawn(async move {
-                            if let Err(e) = handle_connection(stream).await {
-                                tracing::error!("Connection error: {}", e);
-                            }
-                        });
+                    Ok(Ok((n, fds))) => {
+                        tracing::debug!("Received {} bytes, {} fds", n, fds);
+                    }
+                    Ok(Err(e)) => {
+                        tracing::error!("Receive error: {}", e);
                     }
                     Err(e) => {
-                        tracing::error!("Accept error: {}", e);
+                        tracing::error!("Task error: {}", e);
                     }
                 }
             }
@@ -45,16 +57,5 @@ pub async fn run_daemon() -> Result<()> {
     tracing::info!("Cleaning up socket: {}", socket_path);
     std::fs::remove_file(&socket_path)?;
 
-    Ok(())
-}
-
-async fn handle_connection(stream: tokio::net::UnixStream) -> anyhow::Result<()> {
-    tracing::debug!("Handling connection");
-    let mut buf = [0u8; 1024];
-    let mut fd_storage = [0; 8];
-    let (n, fds) =
-        tokio::task::spawn_blocking(move || stream.recv_with_fd(&mut buf, &mut fd_storage))
-            .await??;
-    tracing::debug!("Received {} bytes, {} fds", n, fds);
     Ok(())
 }
